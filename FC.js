12ae930e2d921e64ab88165d6e98bf8ca443560e
BenCat07 <BenCat07@gmail.com>
2017-08-06 21:49:06 +0200

    Add files via upload

--- FC.js
+++ FC.js
@@ -23,47 +23,51 @@ let saveData = () => {
 };
 bot.on('ready', () => {
   console.log('My body is Reggie');
-  //bot.user.setGame('+FC')
+  bot.user.setGame('!FC')
   console.log(`Connected as ${bot.user.tag}.`);
 });
-bot.on('disconnect', () => {
-  console.log('Disconnected.')
-})
 bot.on('reconnecting', () => {
   console.log('Reconnecting...')
 })
-var prefix = "+"
+bot.on('disconnect', () => {
+  console.log('Disconnected.')
+})
+
+var prefix = "!"
+
 bot.on('message', message => {
 	if (message.author === bot.user) return;
 	const userID = message.author.id;
+	const guildID = message.guild.id;
 	const person = message.mentions.members.first();
-	if (!data[userID]) {
-    	data[userID] = Object.assign({}, base);
+	if (!data[guildID]) {
+    	data[guildID] = Object.assign({}, base);
     	saveData();
   	}
 	if (!data[userID].FC) {
     	data[userID].FC = "";
     	saveData();
   	}
-      if (!data[userID].IGN) {
-    	data[userID].IGN = "";
-    	saveData();
-  	}
-    if (message.content.startsWith(prefix + "fc") || message.content.startsWith(prefix + "FC")) {
-        if (message.content.split(" ")[1] == undefined) {
-            message.channel.send("Usage:\n+FC (FC IGN)\nAdds Your FC and IGN to the bot so others can see it with +FC @YourName")
-
+    if (message.content.startsWith(prefix + "FC")) {
+        if (!person && message.content.split(" ")[1] == (undefined || null)) {
+            message.channel.send("Usage:\n+FC (FC IGN)\nAdds Your FC to the bot so others can see it with +FC @YourName")
         }
-        else if (!person) {
+        else if (!person) {    
             data[userID].FC = message.content.split(" ")[1]
-            data[userID].IGN = message.content.split(" ")[2]
             saveData();
-            message.channel.send("Your FC is now " + message.content.split(" ")[1] +" and your IGN is now " + message.content.split(" ")[2])
+            message.channel.send("Your FC is now " + data[userID].FC)
         }
         else if (person) {
-            message.channel.send(person+"'s FC is: "+data[person.id].FC+" and his IGN is "+data[person.id].IGN)
+            if (data[person.id] == undefined || data[person.id].FC == undefined || data[person.id].FC == "") {
+                message.channel.send(`${person} hasn\'t registered himself in the database yet.`)
+                return;
+            }
+            message.channel.send(data[person.id].FC)
         }
     }
-});
 
-bot.login(config.token); 
+});
+process.on("unhandledRejection", err => {
+  console.error("Uncaught Promise Error:\n" + err.stack)
+})
+bot.login('config.token'); 
